6f984a78cd2f36dd955548fa3de1f1216da84906
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2024-12-03 10:19:47 +0600

    Intialized

---
+++ manage.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+struct movie
+{
+    char title[100];
+    char director[50];
+    int release_year;
+    float rating;
+};
+
+int add(char *title,char *dir,int year,int rating){
+    
+}
+void display();
+void search();
+int update();
