1ec81f9107dbbcff0cb5aa9bd7a7668d5844f79c
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2024-12-04 21:11:22 +0600

    Added Movie Add feature

--- main.c
+++ main.c
@@ -3,18 +3,40 @@
 #include "manage.c"
 
 int main(){
-    const char *welcome_screen = "/* .-----------------------------------------------------------------------------. */\n"
-    "/* |__        __   _                            _____                            | */\n"
-    "/* |\\ \\      / /__| | ___ ___  _ __ ___   ___  |_   _|__                         | */\n"
-    "/* | \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\   | |/ _ \\                        | */\n"
-    "/* |  \\ V  V /  __/ | (_| (_) | | | | | |  __/   | | (_) |                       | */\n"
-    "/* | _ \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___| _ |_|\\___/_  __            _      | */\n"
-    "/* || |    ___ _ __ ___   ___  _ __   __ _  __| | ___  |  \\/  | _____   _(_) ___ | */\n"
-    "/* || |   / _ \\ '_ ` _ \\ / _ \\| '_ \\ / _` |/ _` |/ _ \\ | |\\/| |/ _ \\ \\ / / |/ _ \\| */\n"
-    "/* || |__|  __/ | | | | | (_) | | | | (_| | (_| |  __/ | |  | | (_) \\ V /| |  __/| */\n"
-    "/* ||_____\\___|_| |_| |_|\\___/|_| |_|\\__,_|\\__,_|\\___| |_|  |_|\\___/ \\_/ |_|\\___|| */\n"
-    "/* '-----------------------------------------------------------------------------' */\n";
-    printf("%s\n",welcome_screen);
-    struct movie myMovie;
+    int choice;
+    while(1){
+        clear();
+        menu();
+        if(scanf("%d", &choice)==EOF){
+            printf("\nBye :)\n");
+            exit(0);
+        } else if((choice>2&&choice<31)||(choice>32&&choice<40)||choice>44||choice==0){
+            printf("Invalid Choice\n");
+            fflush(stdin);
+            pause();
+        } else {
+            fflush(stdin);
+            if(choice==1){ // Add Movie
+                struct movie myMovie;
+                printf("Enter Movie Name: ");
+                fgets(myMovie.title,sizeof(myMovie.title),stdin);
+                myMovie.title[strcspn(myMovie.title,"\n")] = '\0';
+                printf("Enter Movie Director Name: ");
+                fgets(myMovie.director,sizeof(myMovie.director),stdin);
+                myMovie.director[strcspn(myMovie.director,"\n")] = '\0';
+                printf("Enter Movie Year: ");
+                scanf("%d", &myMovie.release_year);
+                printf("Enter Movie Rating: ");
+                scanf("%f", &myMovie.rating);
+                add(myMovie.title,myMovie.director,myMovie.release_year,myMovie.rating);
+            } else if(choice==2){
+                continue;
+            } else if(choice==3){
+                continue;
+            } else if(choice>=40){
+                continue;
+            }
+        }
+    }
     return 0;
 }
--- manage.c
+++ manage.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
 
+void clear(){
+    system("@cls||clear");
+}
+void pause(){
+    printf("Press any key to continue...");
+    getch();
+}
 struct movie
 {
     char title[100];
@@ -8,8 +18,26 @@ struct movie
     float rating;
 };
 
-int add(char *title,char *dir,int year,int rating){
-    
+void menu(){
+    char menu[] = "\n[1] Add Movie\n"
+    "[2] Display Movie\n"
+    "[3] Search Movie\n"
+    "\t[1] Title\n\t[2] Director\n"
+    "[4] Update Movie Details\n"
+    "\t[1] Title\n\t[2] Director\n\t[3] Release Year\n\t[4] Rating\n\t[0] All\n"
+    "[To Exit Press Ctrl-c]\n"
+    ">> ";
+    printf("%s", menu);
+}
+void add(char title[100],char dir[50],int year,float rating){
+    char result[1000];
+    FILE *f; char content [1000];
+    snprintf(result,sizeof(result),"%s;%s;%d;%.1f",title,dir,year,rating);
+    f = fopen("film.txt", "a+");
+    fputs(result,f);
+    fclose(f);
+    printf("%s has successfully added to Database\n", title);
+    pause();
 }
 void display();
 void search();
