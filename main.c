89a37b401b9a8488d05c0697078d83d923a45a5b
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2024-12-05 21:50:17 +0600

    Added Search & Update Feature

--- main.c
+++ main.c
@@ -1,17 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include "manage.c"
+#include "utils.c"
 
 int main(){
+    char dir[1024];
+    getcwd(dir,sizeof(dir));
     int choice;
     while(1){
         clear();
-        menu();
-        if(scanf("%d", &choice)==EOF){
+        menu(dir);
+        int input = scanf("%d", &choice);
+        if(input==EOF){
             printf("\nBye :)\n");
             exit(0);
-        } else if((choice>2&&choice<31)||(choice>32&&choice<40)||choice>44||choice==0){
-            printf("Invalid Choice\n");
+        } else if(input!=1){
+            printf("Invalid Input\n");
+            fflush(stdin);
+            pause();
+        } else if((choice>4&&choice<31)||(choice>32&&choice<41)||choice>44){
+            printf("Invalid Input\n");
             fflush(stdin);
             pause();
         } else {
@@ -29,13 +34,66 @@ int main(){
                 printf("Enter Movie Rating: ");
                 scanf("%f", &myMovie.rating);
                 add(myMovie.title,myMovie.director,myMovie.release_year,myMovie.rating);
-            } else if(choice==2){
+                pause();
+            } else if(choice==2){ // Display All Movie
                 clear();
                 display();
-            } else if(choice==3){
-                continue;
-            } else if(choice>=40){
-                continue;
+                pause();
+            } else if(choice==3){ // Search Menu
+                clear();
+                search_menu(dir);
+                int search_option;
+                scanf("%d", &search_option);
+                if(search_option==1){
+                    clear();
+                    search(search_option); // title
+                    pause();
+                } else if(search_option==2){
+                    clear();
+                    search(search_option); // director
+                    pause();
+                } else if(search_option==0){
+                    printf("\nBye :)\n");
+                    exit(0);
+                } else {
+                    printf("Invalid Option\n");
+                    pause();
+                }
+            } else if(choice==4){ // Update Menu
+                clear();
+                update_menu(dir);
+                int search_option;
+                scanf("%d", &search_option);
+                if(search_option>0&&search_option<5){
+                    clear();
+                    update(search_option);
+                } else if(search_option==0){
+                    printf("\nBye :)\n");
+                    exit(0);
+                } else {
+                    printf("Invalid Option\n");
+                }
+                pause();
+            } else if(choice>30&&choice<33){
+                if(choice==31){
+                    search(1);
+                    pause();
+                } else if(choice==32){
+                    search(2);
+                    pause();
+                }
+            } else if(choice>40&&choice<45){
+                clear();
+                switch(choice){
+                    case 41: update(1); break;
+                    case 42: update(2); break;
+                    case 43: update(3); break;
+                    case 44: update(4); break;
+                }
+                pause();
+            } else if(choice==0){
+                printf("\nBye :)\n");
+                exit(0);
             }
         }
     }
